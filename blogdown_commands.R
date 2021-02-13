b4a0c6907741468fda2b7c4c72cc49a1a14eb41d
Ruzica <ruzica.gajic.7512@gmail.com>
2021-02-13 12:34:01 +0100

    minor changes

--- blogdown_commands.R
+++ blogdown_commands.R
@@ -1,14 +1,12 @@
-install.packages('blogdown')
-library(blogdown)
-setwd("/Users/ruzicagajic/Documents/Data Scientist/r-website/r-website/content/post/2021-02-05 COVID-19 mobility trends by Apple")
-
 
 if (!require("rmarkdown"))
 install.packages("rmarkdown")
 
-
 if (!require("pacman"))
 install.packages("pacman")
+
+library(blogdown)
+setwd("/Users/ruzicagajic/Documents/Data Scientist/r-website/r-website/content/post/2021-02-05 COVID-19 mobility trends by Apple")
 
 blogdown::hugo_version()
 blogdown::update_hugo()
